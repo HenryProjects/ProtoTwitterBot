82f7815af6ab5c5242f2b8d7a744d88fa96189e8
Sean.Henry <fidelicatessen@gmail.com>
2017-02-20 21:22:53 -0800

    Add virtual env and tweepy

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\Users\Owner\desktop\prototwitterbot\venv"
+set "VIRTUAL_ENV=C:\Users\Sean\prototwitterbot\venv"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
