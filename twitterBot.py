20e0dc1a31758132e6f8c01923f2a81307033ef1
Sean.Henry <fidelicatessen@gmail.com>
2017-03-04 17:40:36 -0800

    Change insult string

--- twitterBot.py
+++ twitterBot.py
@@ -203,7 +203,7 @@ def insult_generator():
     c2 = random.randint(0, 61)
     c3 = random.randint(0, 59)
 
-    insult = "Thou " + column1[c1] + " " + column2[c2] + " " + column3[c3] + "!"
+    insult = ".@realdonaldtrump Thou " + column1[c1] + " " + column2[c2] + " " + column3[c3] + "!"
     return insult
 
 
